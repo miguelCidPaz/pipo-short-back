fd1090413cae777670ac67e4a244b76c59755323
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-04-04 20:57:07 +0200

    Basic structure

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const http = require("http");
-const dotenv = require('dotenv').config();
-const hostname = "127.0.0.1";
-const port = 3000;
+const app = require("express")();
+app.use(require("express").json());
+app.use('/pipo-back', require("./routes"));
 
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
-
-
-const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.uzoxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
-  if (err) console.log("Error occurred connecting to MongoDB...");
-  console.log("Connected to MongoDB!");
-});
-
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, () => {
+    console.log('Working!!')
+})
