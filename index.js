9b09a4d837b4ce6b8ab9b1ee59ecb9eb2701643a
Miguel Cid <miguel.cid.paz@gmail.com>
2022-04-04 19:18:37 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const dotenv = require('dotenv').config();
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+console.log(process.env.MONGODB_USER)
+console.log(process.env.MONGODB_PASS)
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.uzoxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+MongoClient.connect(uri, {useUnifiedTopology: true }, (err, client) => {
+  if (err) console.log("Error occurred connecting to MongoDB...");
+  console.log("Connected to MongoDB!");
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
