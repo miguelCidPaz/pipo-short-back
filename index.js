2c362216edfc355ff5678f1abebf1d156de37654
Miguel Cid <miguel.cid.paz@gmail.com>
2022-04-04 19:25:58 +0200

    init

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require("http");
 const dotenv = require('dotenv').config();
 const hostname = "127.0.0.1";
 const port = 3000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World\n");
 });
-console.log(process.env.MONGODB_USER)
-console.log(process.env.MONGODB_PASS)
+
 const MongoClient = require("mongodb").MongoClient;
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.uzoxg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 MongoClient.connect(uri, {useUnifiedTopology: true }, (err, client) => {
